c3a22dc11df063a24063feadc5f811292f06715e
Josh Dev <1900843+JoshDev@users.noreply.github.com>
2021-09-08 21:15:03 -0400

    Update Feed

--- src/Feed.js
+++ src/Feed.js
@@ -6,26 +6,34 @@ import EventNoteIcon from "@material-ui/icons/EventNote";
 import React from "react";
 import "./Feed.css";
 import InputOption from "./InputOption";
+import Post from "./Post";
 
 function Feed() {
-  return <div className="feed">
+  return (
+    <div className="feed">
       <div className="feed__inputContainer">
-          <div className="feed__input">
-              <CreateIcon />
-              <form >
-                  <input type="text" />
-                  <button type='submit'>Send</button>
-                  </form>
-          </div>
-          <div className="feed__inputOptions">
-              <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
-              <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
-              <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
-              <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
-
-          </div>
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
+        </div>
       </div>
-  </div>;
+      {/* Posts */}
+      <Post name="Josh Dev" description="This is a test" />
+    </div>
+  );
 }
 
 export default Feed;
